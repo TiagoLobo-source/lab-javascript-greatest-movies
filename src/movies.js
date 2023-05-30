9613781e8fa4e0a59854fed2a1cd287c6c840205
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-05-30 17:40:00 +0100

    need to revise lab

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,101 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
-
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
-
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
-
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map(movie => movie.director);
+  }
+  
+  const onlyDrama = moviesArray => moviesArray.filter(movie => movie.genre.includes("Drama"));
+  
+  function howManyMovies(moviesArray) {
+    const stevenDramaMovies = moviesArray.filter(movie =>
+      movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+    return stevenDramaMovies.length;
+  }
+  
+  function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0) {
+      return 0;
+    }
+  
+    const sum = moviesArray.reduce((total, current) => {
+      if (typeof current.score === "number") {
+        return total + current.score;
+      }
+      return total;
+    }, 0);
+  
+    const average = sum / moviesArray.length;
+    const decimals = Math.round(average * 100) / 100;
+    return decimals;
+  }
+  
+  function dramaMoviesScore(moviesArray) {
+    const filteredByDrama = moviesArray.filter(movie =>
+      movie.genre.includes("Drama")
+    );
+    return scoresAverage(filteredByDrama);
+  }
+  
+  function orderByYear(moviesArray) {
+    const copy = [...moviesArray];
+  
+    copy.sort((a, b) => {
+      if (a.year === b.year) {
+        return a.title.localeCompare(b.title);
+      } else {
+        return a.year - b.year;
+      }
+    });
+    return copy;
+  }
+  
+  function orderAlphabetically(moviesArray) {
+    const sortedTitles = moviesArray.map(movie => movie.title)
+      .sort((a, b) => a.localeCompare(b));
+  
+    return sortedTitles.slice(0, 20);
+  }
+  
+  function turnHoursToMinutes(moviesArray) {
+    return moviesArray.map(movie => {
+      const duration = movie.duration.split(" ");
+      let totalMinutes = 0;
+  
+      for (let timeFragment of duration) {
+        if (timeFragment.includes("h")) {
+          totalMinutes += parseInt(timeFragment) * 60;
+        } else if (timeFragment.includes("min")) {
+          totalMinutes += parseInt(timeFragment);
+        }
+      }
+  
+      return { ...movie, duration: totalMinutes };
+    });
+  }
+  
+  function bestYearAvg(moviesArray) {
+    if (moviesArray.length === 0) {
+      return null;
+    }
+  
+    const scoresByYear = {};
+    for (let movie of moviesArray) {
+      if (!(movie.year in scoresByYear)) {
+        scoresByYear[movie.year] = [];
+      }
+      scoresByYear[movie.year].push(movie.score);
+    }
+  
+    let bestYear = null;
+    let bestAverage = 0;
+  
+    for (let year in scoresByYear) {
+      const average = scoresByYear[year].reduce((total, score) => total + score, 0) / scoresByYear[year].length;
+  
+      if (average > bestAverage || (average === bestAverage && year < bestYear)) {
+        bestYear = year;
+        bestAverage = average;
+      }
+    }
+  
+    return `The best year was ${bestYear} with an average score of ${bestAverage.toFixed(2)}`;
+  }
